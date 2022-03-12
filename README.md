f05933837f55231e7884f5e6d6f95d9a71a58a9f
beer <beer5214@126.com>
2022-03-12 08:57:05 +0800

    create metmuseum dir

--- README.md
+++
@@ -1,7 +0,0 @@
-
-
-> Metmuseum 数据采集
-
-
-
-https://beer5214.notion.site/metmuseum-004a9ccc330445daa9740527fd94701a
--- sync.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -ex
-
-base_dir='/Users/beer/beer/metmuseum'
-
-rsync -avzP $base_dir/* hdd:/home/zhanglong/code --exclude-from="$base_dir/exclude.list"
